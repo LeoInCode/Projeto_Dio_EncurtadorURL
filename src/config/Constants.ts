46f2defef0d95200ed5b57a9362b224a2b5b71e0
LeoInCode <lsm.ccomp@gmail.com>
2020-10-19 15:33:44 -0300

    Configurando projeto

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+	"mongodb+srv://<username>:<password>@url-shortener-dio.vtxzo.mongodb.net/<dbname>?retryWrites=true&w=majority"
 }
